f13d75061a49e56bd459534a2a2b10da0aa74076
MiguelRony <miguel.rony@icloud.com>
2025-01-22 00:06:33 -0600

    add functions reverseString, calculator, caesarCipher and analyzeArrays with respectives tests

--- practice-test.test.js
+++ practice-test.test.js
@@ -1,5 +1,29 @@
-import { capitalize } from "./practice";
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./practice";
 
 test("capitalize", () => {
   expect(capitalize("hello")).toBe("Hello");
+});
+
+test("reverseString", () => {
+    expect(reverseString("hello")).toBe("olleh");
+});
+
+test("calculator", () => {
+    expect(calculator.add(1, 2)).toBe(3);
+    expect(calculator.subtract(2, 1)).toBe(1);
+    expect(calculator.multiply(2, 3)).toBe(6);
+    expect(calculator.divide(6, 2)).toBe(3);
+});
+
+test("caesarCipher", () => {
+    expect(caesarCipher("Hello, World!", 5)).toBe("Mjqqt, Btwqi!");
+});
+
+test("analyzeArray", () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+    });
 });
--- practice.js
+++ practice.js
@@ -3,3 +3,40 @@
 export const capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const reverseString = (str) => {
+  return str.split("").reverse().join("");
+}
+
+export const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+}
+
+export const caesarCipher = (str, shift) => {
+    return str
+        .split("")
+        .map(char => {
+            if (char.match(/[a-z]/i)) {
+                const code = char.charCodeAt(0);
+                if (code >= 65 && code <= 90) {
+                    return String.fromCharCode(((code - 65 + shift) % 26) + 65);
+                } else if (code >= 97 && code <= 122) {
+                    return String.fromCharCode(((code - 97 + shift) % 26) + 97);
+                }
+            }
+            return char;
+        })
+        .join("");
+}
+
+export const analyzeArray = (array) => {
+    return {
+        average: array.reduce( (prev, curr) => prev + curr) / array.length,
+        min: Math.min(...array),
+        max: Math.max(...array),
+        length: array.length
+    }
+}
